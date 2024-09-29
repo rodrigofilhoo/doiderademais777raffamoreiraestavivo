1f60cf2cb905a8103868262c2bf3989234fcc3c4
Rodrigo Filho <rodrigofilhogt@gmail.com>
2024-09-29 19:19:08 -0300

    -+

--- src/view/Cart.vue
+++ src/view/Cart.vue
@@ -18,19 +18,20 @@
                 </div>
                 <div class="col-md-3 col-lg-3 col-xl-3">
                     <p class="lead fw-normal mb-2">Nome produto</p>
+
                 </div>
                 <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-danger px-2"
                     onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                    <i class="fas fa-minus"></i>
+                    -
                     </button>
 
                     <input id="form1" min="0" name="quantity" value="2" type="number"
                     class="form-control form-control-sm" />
 
-                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn px-2"
                     onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                    <i class="fas fa-plus"></i>
+                    +
                     </button>
                 </div>
                 <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
